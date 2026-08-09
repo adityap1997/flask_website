93d7baee31423eebc2060ea0d48eeb4b8bd8f3f7
adityap1997 <154703824+adityap1997@users.noreply.github.com>
2026-08-09 16:04:52 +0530

    Change default port from 5500 to 5000

--- app.py
+++ app.py
@@ -26,5 +26,5 @@ def catch_all(path):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5500))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
