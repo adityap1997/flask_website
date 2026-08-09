5fee6ef4aa8b00194aed6e1c578b50d05bcd96f6
adityap1997 <154703824+adityap1997@users.noreply.github.com>
2026-08-09 15:47:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,5 +28,5 @@ def catch_all(path):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5500))
     app.run(host="0.0.0.0", port=port)
