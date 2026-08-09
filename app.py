0e422033e085fdd9b486905b3627bfe9b5270c9d
adityap1997 <154703824+adityap1997@users.noreply.github.com>
2026-08-09 16:07:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,24 @@
 from flask import Flask, send_from_directory
 import os
 
-# Path to the static folder
-STATIC_DIR = os.path.join(os.path.dirname(__file__), "static")
+app = Flask(__name__)
 
-app = Flask(
-    __name__
-    )
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 @app.route("/")
 def home():
-    return send_from_directory("index.html")
+    return send_from_directory(BASE_DIR, "index.html")
 
 
-# Catch-all route for files/pages inside static
 @app.route("/<path:path>")
 def catch_all(path):
-    full_path = os.path.join(path)
+    full_path = os.path.join(BASE_DIR, path)
 
     if os.path.isfile(full_path):
-        return send_from_directory(path)
+        return send_from_directory(BASE_DIR, path)
 
-    return send_from_directory("index.html")
+    return send_from_directory(BASE_DIR, "index.html")
 
 
 if __name__ == "__main__":
